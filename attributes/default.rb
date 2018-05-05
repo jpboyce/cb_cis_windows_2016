2c4fb28f28cecbc5231260892d9351d1335fc9de
Jesse Boyce <jesse@jpboyce.org>
2018-05-05 15:47:59 +1000

    created file

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+# Default attributes
+
+# CIS Levels
+default['cb_cis_windows_2016']['cis_level_1'] = true
+default['cb_cis_windows_2016']['cis_level_2'] = true
+
+
+# Server Roles
+default['cb_cis_windows_2016']['is_member_server'] = true
+default['cb_cis_windows_2016']['is_domain_controller'] = false
