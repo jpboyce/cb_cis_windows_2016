94784d4075851b1806d8144661e8593dc61aba73
Jesse Boyce <jesse@jpboyce.org>
2018-05-17 19:59:00 +1000

    Minor fixes and powershell script

--- recipes/cis-9-1-domain-profile.rb
+++ recipes/cis-9-1-domain-profile.rb
@@ -4,71 +4,92 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # 9.1.1 (L1) Ensure 'Windows Firewall: Domain: Firewall state' is set to 'On (recommended)'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\EnableFirewall ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'EnableFirewall', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.2 (L1) Ensure 'Windows Firewall: Domain: Inbound connections' is set to 'Block (default)'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\DefaultInboundAction ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'DefaultInboundAction', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.3 (L1) Ensure 'Windows Firewall: Domain: Outbound connections' is set to 'Allow (default)'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\DefaultOutboundAction ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'DefaultOutboundAction', type: :dword, data: 0 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.4 (L1) Ensure 'Windows Firewall: Domain: Settings: Display a notification' is set to 'No'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\DisableNotifications ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'DisableNotifications', type: :dword, data: 0 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.5 (L1) Ensure 'Windows Firewall: Domain: Settings: Apply local firewall rules' is set to 'Yes (default)'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\AllowLocalPolicyMerge ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'AllowLocalPolicyMerge', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.6 (L1) Ensure 'Windows Firewall: Domain: Settings: Apply local connection security rules' is set to 'Yes (default)'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\AllowLocalIPsecPolicyMerge ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile' do
+  values [{ name: 'AllowLocalIPsecPolicyMerge', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.7 (L1) Ensure 'Windows Firewall: Domain: Logging: Name' is set to '%SYSTEMROOT%\System32\logfiles\firewall\domainfw.log'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging\LogFilePath ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging' do
+  values [{ name: 'LogFilePath', type: :string, data: '%SYSTEMROOT%\System32\logfiles\firewall\domainfw.log' }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.8 (L1)  Ensure 'Windows Firewall: Domain: Logging: Size limit (KB)' is set to '16,384 KB or greater'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging\LogFileSize ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging' do
+  values [{ name: 'LogFileSize', type: :dword, data: 16384 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.9 (L1) Ensure 'Windows Firewall: Domain: Logging: Log dropped packets' is set to 'Yes'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging\LogDroppedPackets ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging' do
+  values [{ name: 'LogDroppedPackets', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.1.10 (L1) Ensure 'Windows Firewall: Domain: Logging: Log successful connections' is set to 'Yes'
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging\LogSuccessfulConnections ' do
-  values [{ name: '', type: :dword, data: 1 }]
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging' do
+  values [{ name: 'LogSuccessfulConnections', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
+end
+
+powershell_script 'Firewall Domain Profile Settings' do
+  code <<-EOH
+  $params = @{
+    'Name'='Domain';
+    'Enabled'='True';
+    'DefaultInboundAction'='Block';
+    'DefaultOutboundAction'='Allow';
+    'AllowLocalFirewallRules'='True';
+    'AllowLocalIPsecRules'='True';
+    'NotifyOnListen'='False';
+    'LogFileName'='%SYSTEMROOT%\System32\logfiles\firewall\domainfw.log';
+    'LogMaxSizeKilobytes'='16384';
+    'LogAllowed'='True';
+    'LogBlocked'='True';
+    'PolicyStore'="$env:COMPUTERNAME"
+  }
+
+  Set-NetFirewallProfile @params -Verbose
+  EOH
 end
--- recipes/cis-9-3-public-profile.rb
+++ recipes/cis-9-3-public-profile.rb
@@ -5,70 +5,91 @@
 
 # 9.3.1 (L1) Ensure 'Windows Firewall: Public: Firewall state' is set to 'On (recommended)'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'EnableFirewall ', type: :dword, data: 1 }]
+  values [{ name: 'EnableFirewall', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.2 (L1) Ensure 'Windows Firewall: Public: Inbound connections' is set to 'Block (default)'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'DefaultInboundAction ', type: :dword, data: 1 }]
+  values [{ name: 'DefaultInboundAction', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.3 (L1) Ensure 'Windows Firewall: Public: Outbound connections' is set to 'Allow (default)'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'DefaultOutboundAction ', type: :dword, data: 1 }]
+  values [{ name: 'DefaultOutboundAction', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.4 (L1) Ensure 'Windows Firewall: Public: Settings: Display a notification' is set to 'Yes'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'DisableNotifications ', type: :dword, data: 1 }]
+  values [{ name: 'DisableNotifications', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.5 (L1) Ensure 'Windows Firewall: Public: Settings: Apply local firewall rules' is set to 'No'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'AllowLocalPolicyMerge ', type: :dword, data: 1 }]
+  values [{ name: 'AllowLocalPolicyMerge', type: :dword, data: 0 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.6 (L1) Ensure 'Windows Firewall: Public: Settings: Apply local connection security rules' is set to 'No'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile' do
-  values [{ name: 'AllowLocalIPsecPolicyMerge ', type: :dword, data: 1 }]
+  values [{ name: 'AllowLocalIPsecPolicyMerge', type: :dword, data: 0 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.7 (L1) Ensure 'Windows Firewall: Public: Logging: Name' is set to '%SYSTEMROOT%\System32\logfiles\firewall\publicfw.log'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging' do
-  values [{ name: 'LogFilePath ', type: :dword, data: 1 }]
+  values [{ name: 'LogFilePath', type: :string, data: '%SYSTEMROOT%\System32\logfiles\firewall\publicfw.log' }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.8 (L1)  Ensure 'Windows Firewall: Public: Logging: Size limit (KB)' is set to '16,384 KB or greater'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging' do
-  values [{ name: 'LogFileSize ', type: :dword, data: 1 }]
+  values [{ name: 'LogFileSize', type: :dword, data: 16384 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.9 (L1) Ensure 'Windows Firewall: Public: Logging: Log dropped packets' is set to 'Yes'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging' do
-  values [{ name: 'LogDroppedPackets ', type: :dword, data: 1 }]
+  values [{ name: 'LogDroppedPackets', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
 end
 
 # 9.3.10 (L1) Ensure 'Windows Firewall: Public: Logging: Log successful connections' is set to 'Yes'
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging' do
-  values [{ name: 'LogSuccessfulConnections ', type: :dword, data: 1 }]
+  values [{ name: 'LogSuccessfulConnections', type: :dword, data: 1 }]
   action :create
   only_if { node.default['cb_cis_windows_2016']['cis_level_1'] = true }
+end
+
+powershell_script 'Firewall Domain Profile Settings' do
+  code <<-EOH
+  $params = @{
+    'Name'='Public';
+    'Enabled'='True';
+    'DefaultInboundAction'='Block';
+    'DefaultOutboundAction'='Allow';
+    'AllowLocalFirewallRules'='False';
+    'AllowLocalIPsecRules'='False';
+    'NotifyOnListen'='True';
+    'LogFileName'='%SYSTEMROOT%\System32\logfiles\firewall\publicfw.log';
+    'LogMaxSizeKilobytes'='16384';
+    'LogAllowed'='True';
+    'LogBlocked'='True';
+    'PolicyStore'="$env:COMPUTERNAME"
+  }
+
+  Set-NetFirewallProfile @params -Verbose
+  EOH
 end
