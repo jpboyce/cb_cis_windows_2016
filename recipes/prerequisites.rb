af7b3407dac1ddb1d4a0835d3aebbd9687470129
Jesse Boyce <jesse@jpboyce.org>
2018-05-27 07:31:28 +1000

    Added guards

--- recipes/prerequisites.rb
+++ recipes/prerequisites.rb
@@ -7,10 +7,12 @@
 cookbook_file 'C:\Windows\PolicyDefinitions\MSS-legacy.admx' do
   source 'MSS-legacy.admx'
   action :create
+  only_if { node['cb_cis_windows_2016']['copy_mss'] }
 end
 
 # Copy MSS ADML file
 cookbook_file 'C:\Windows\PolicyDefinitions\en-US\MSS-legacy.adml' do
   source 'MSS-legacy.adml'
   action :create
+  only_if { node['cb_cis_windows_2016']['copy_mss'] }
 end
