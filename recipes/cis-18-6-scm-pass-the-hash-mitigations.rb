0292b47d221dfb14681364a3610d53d446a05e8d
Jesse Boyce <jesse@jpboyce.org>
2018-06-02 19:54:09 +1000

    Added a guard to prevent 18.6.1 running under test kitchen

--- recipes/cis-18-6-scm-pass-the-hash-mitigations.rb
+++ recipes/cis-18-6-scm-pass-the-hash-mitigations.rb
@@ -2,12 +2,12 @@
 # Recipe:: cis-18-6-scm-pass-the-hash-mitigations
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
 # 18.6.1 (L1) Ensure 'Apply UAC restrictions to local accounts on network logons' is set to 'Enabled' (MS only)
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' do
   values [{ name: 'LocalAccountTokenFilterPolicy', type: :dword, data: 0 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  not_if { ENV['TEST_KITCHEN'].to_i == 1 }
 end
 
 # 18.6.2 (L1) Ensure 'WDigest Authentication' is set to 'Disabled'
