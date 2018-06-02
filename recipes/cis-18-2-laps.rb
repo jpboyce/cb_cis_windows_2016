69cc4e681ec4b8cc8d59835fb31ba3841e802ac6
Jesse Boyce <jesse@jpboyce.org>
2018-06-03 05:31:05 +1000

    Added guard to only run if LAPS extension is installed

--- recipes/cis-18-2-laps.rb
+++ recipes/cis-18-2-laps.rb
@@ -4,17 +4,18 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 # only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 # 18.2.1 (L1) Ensure LAPS AdmPwd GPO Extension / CSE is installed (MS only)
-registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D-087DE603E3EA}' do
-  values [{ name: 'DllName', type: :dword, data: 1 }]
-  action :create
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D-087DE603E3EA}' do
+#  values [{ name: 'DllName', type: :dword, data: 1 }]
+#  action :create
+#  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+# end
 
 # 18.2.2 (L1) Ensure 'Do not allow password expiration time longer than required by policy' is set to 'Enabled' (MS only)
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
   values [{ name: 'PwdExpirationProtectionEnabled', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 end
 
 # 18.2.3 (L1) Ensure 'Enable Local Admin Password Management' is set to 'Enabled' (MS only)
@@ -22,6 +23,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
   values [{ name: 'AdmPwdEnabled', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 end
 
 # 18.2.4 (L1) Ensure 'Password Settings: Password Complexity' is set to 'Enabled: Large letters + small letters + numbers + special characters' (MS only)
@@ -29,6 +31,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
   values [{ name: 'PasswordComplexity', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 end
 
 # 18.2.5 (L1) Ensure 'Password Settings: Password Length' is set to 'Enabled: 15 or more' (MS only)
@@ -36,6 +39,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
   values [{ name: 'PasswordLength', type: :dword, data: 15 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 end
 
 # 18.2.6 (L1) Ensure 'Password Settings: Password Age (Days)' is set to 'Enabled: 30 or fewer' (MS only)
@@ -43,4 +47,5 @@ registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
   values [{ name: 'PasswordAgeDays', type: :dword, data: 30 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { registry_key_exists?('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D087DE603E3EA}', :x86_x64) }
 end
