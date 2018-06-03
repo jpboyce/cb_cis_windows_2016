d6e555d3f409cb92739ffb8fbd56923727751687
Jesse Boyce <jesse@jpboyce.org>
2018-06-03 10:39:08 +1000

    Compressed down to one resource block

--- recipes/cis-1-1-password-policy.rb
+++ recipes/cis-1-1-password-policy.rb
@@ -13,41 +13,16 @@ security_policy 'Enforce password history' do
 end
 
 # 1.1.2 (L1)  Ensure 'Maximum password age' is set to '60 or fewer days, but not 0'
-security_policy 'Maximum password age' do
-  policy_template "#{node['cb_cis_windows_2016']['secedit_template']['location']}\\Maximum_password_age.inf"
-  database "#{node['cb_cis_windows_2016']['secedit_database']['location']}\\#{node['cb_cis_windows_2016']['secedit_database']['name']}"
-  action :configure
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# This is a placeholder entry.  This setting is applied in the cis-1-1-password-policy cookbook via the security_policy resource
 
 # 1.1.3 (L1) Ensure 'Minimum password age' is set to '1 or more day(s)'
-security_policy 'Minimum password age' do
-  policy_template "#{node['cb_cis_windows_2016']['secedit_template']['location']}\\Minimum_password_age.inf"
-  database "#{node['cb_cis_windows_2016']['secedit_database']['location']}\\#{node['cb_cis_windows_2016']['secedit_database']['name']}"
-  action :configure
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# This is a placeholder entry.  This setting is applied in the cis-1-1-password-policy cookbook via the security_policy resource
 
 # 1.1.4 (L1) Ensure 'Minimum password length' is set to '14 or more character(s)'
-security_policy 'Minimum password length' do
-  policy_template "#{node['cb_cis_windows_2016']['secedit_template']['location']}\\Minimum_password_length.inf"
-  database "#{node['cb_cis_windows_2016']['secedit_database']['location']}\\#{node['cb_cis_windows_2016']['secedit_database']['name']}"
-  action :configure
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# This is a placeholder entry.  This setting is applied in the cis-1-1-password-policy cookbook via the security_policy resource
 
 # 1.1.5 (L1) Ensure 'Password must meet complexity requirements' is set to 'Enabled'
-security_policy 'Password must meet complexity requirements' do
-  policy_template "#{node['cb_cis_windows_2016']['secedit_template']['location']}\\Password_must_meet_complexity_requirements.inf"
-  database "#{node['cb_cis_windows_2016']['secedit_database']['location']}\\#{node['cb_cis_windows_2016']['secedit_database']['name']}"
-  action :configure
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# This is a placeholder entry.  This setting is applied in the cis-1-1-password-policy cookbook via the security_policy resource
 
 # 1.1.6 (L1) Ensure 'Store passwords using reversible encryption' is set to 'Disabled'
-security_policy 'Store passwords using reversible encryption' do
-  policy_template "#{node['cb_cis_windows_2016']['secedit_template']['location']}\\Store_passwords_using_reversible_encryptiony.inf"
-  database "#{node['cb_cis_windows_2016']['secedit_database']['location']}\\#{node['cb_cis_windows_2016']['secedit_database']['name']}"
-  action :configure
-  only_if { node['cb_cis_windows_2016']['cis_level_1'] }
-end
+# This is a placeholder entry.  This setting is applied in the cis-1-1-password-policy cookbook via the security_policy resource
