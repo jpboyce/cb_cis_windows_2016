c1c552c49a93721bd7b80a361a3e8ff1129a1985
Jesse Boyce <jesse@jpboyce.org>
2018-06-03 10:40:05 +1000

    Added guards for items

--- recipes/cis-2-3-5-domain-controllers.rb
+++ recipes/cis-2-3-5-domain-controllers.rb
@@ -8,6 +8,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa' do
   values [{ name: 'SubmitControl', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { node['cb_cis_windows_2016']['is_domain_controller'] }
 end
 
 # 2.3.5.2 (L1) Ensure 'Domain controller: LDAP server signing requirements' is set to 'Require signing' (DC only)
@@ -15,6 +16,7 @@ registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NTDS\Paramete
   values [{ name: 'LDAPServerIntegrity', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { node['cb_cis_windows_2016']['is_domain_controller'] }
 end
 
 # 2.3.5.3 (L1) Ensure 'Domain controller: Refuse machine account password changes' is set to 'Disabled' (DC only)
@@ -22,4 +24,5 @@ registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Netlogon\Para
   values [{ name: 'RefusePasswordChange', type: :dword, data: 1 }]
   action :create
   only_if { node['cb_cis_windows_2016']['cis_level_1'] }
+  only_if { node['cb_cis_windows_2016']['is_domain_controller'] }
 end
