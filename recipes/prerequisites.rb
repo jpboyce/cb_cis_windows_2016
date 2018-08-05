5efd03d1fbab72217127d8ffd2f9df451a97a08d
Jesse Boyce <jesse@jpboyce.org>
2018-08-05 18:56:35 +1000

    Added comment block and placeholder to install wireshark

--- recipes/prerequisites.rb
+++ recipes/prerequisites.rb
@@ -2,7 +2,7 @@
 # Recipe:: prerequisites
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
+=begin
 # Copy MSS ADMX file
 cookbook_file 'C:\Windows\PolicyDefinitions\MSS-legacy.admx' do
   source 'MSS-legacy.admx'
@@ -16,3 +16,9 @@ cookbook_file 'C:\Windows\PolicyDefinitions\en-US\MSS-legacy.adml' do
   action :create
   only_if { node['cb_cis_windows_2016']['copy_mss'] }
 end
+
+#cookbook_file 'c:\users\vagrant\desktop\Wireshark-win64-2.6.1.exe' do
+#source 'Wireshark-win64-2.6.1.exe.zip'
+#action :create
+#end
+=end
