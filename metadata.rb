03852c8af8ed814ed3c8f6e1bac010e49cdfe99f
Jesse Boyce <jesse@jpboyce.org>
2018-09-28 06:51:03 +1000

    Formatting

--- metadata.rb
+++ metadata.rb
@@ -1,26 +1,13 @@
-#
-name 'cb_cis_windows_2016'
-maintainer 'Jesse Boyce'
-maintainer_email 'jesse@jpboyce.org'
-license 'MIT'
-description 'Installs/Configures cb_cis_windows_2016'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://github.com/jpboyce/cb_cis_windows_2016/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-source_url 'https://github.com/jpboyce/cb_cis_windows_2016'
-
-supports 'windows'
-
-depends 'windows', '~> 4.0.0'
-depends 'windows-security-policy', '~> 0.3.7'
+name              'cb_cis_windows_2016'
+maintainer        'Jesse Boyce'
+maintainer_email  'jesse@jpboyce.org'
+license           'MIT'
+description       'Installs/Configures cb_cis_windows_2016'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
+supports          'windows'
+depends           'windows', '~> 4.0.0'
+depends           'windows-security-policy', '~> 0.3.7'
+source_url        'https://github.com/jpboyce/cb_cis_windows_2016'
+issues_url        'https://github.com/jpboyce/cb_cis_windows_2016/issues'
+chef_version      '>= 12.14' if respond_to?(:chef_version)
