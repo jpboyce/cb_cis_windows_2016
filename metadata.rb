c13b8b54b33361ed0ac936893e1899ae7c3206af
Jesse Boyce <jesse@jpboyce.org>
2018-09-28 06:48:00 +1000

    Changed long_description to leverage readme file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'Jesse Boyce'
 maintainer_email 'jesse@jpboyce.org'
 license 'MIT'
 description 'Installs/Configures cb_cis_windows_2016'
-long_description 'Installs/Configures cb_cis_windows_2016'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
