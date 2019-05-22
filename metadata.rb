5c52a09c99e244d213c22f77e2d330bd771bf931
Jesse <jesse@jpboyce.org>
2019-05-23 05:52:55 +1000

    windows version pin to 6 due to Map Collision

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures cb_cis_windows_2016'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
 supports          'windows'
-depends           'windows', '~> 4.0.0'
+depends           'windows', '~> 6.0.0'
 depends           'windows-security-policy', '~> 0.3.7'
 source_url        'https://github.com/jpboyce/cb_cis_windows_2016'
 issues_url        'https://github.com/jpboyce/cb_cis_windows_2016/issues'
